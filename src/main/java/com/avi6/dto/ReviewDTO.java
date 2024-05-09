6f7b8096b1f503a83f3e63aaedfe59619e79cf12
koreavc <koreavc@DESKTOP-UOTUHMA>
2024-05-09 11:33:12 +0900

    ReviewDTO 셋업

--- src/main/java/com/avi6/dto/ReviewDTO.java
+++ src/main/java/com/avi6/dto/ReviewDTO.java
@@ -1,5 +1,34 @@
 package com.avi6.dto;
 
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ReviewDTO {
 
+	private Long reviewNum;
+	
+	//read.html 에 parameter 로 오기 때문에 매핑만 하면 됨.
+	private Long mno;
+	
+	//member id
+	private Long mid;
+	
+	private String nickname;
+	
+	private String email;
+	
+	private int grade;
+	
+	private String text;
+	
+	private LocalDateTime regDate, modDate;
+	
 }
