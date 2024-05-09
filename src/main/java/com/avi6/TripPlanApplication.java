8a12ecb91588674d872106683be91bd3952a1232
koreavc <koreavc@DESKTOP-TCH5SDL>
2024-05-09 10:26:52 +0900

    start

---
+++ src/main/java/com/avi6/TripPlanApplication.java
@@ -0,0 +1,13 @@
+package com.avi6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripPlanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avi6/TripPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avi6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
