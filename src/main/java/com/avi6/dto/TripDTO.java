5cf75fb6f65d4c0786d531f44aa32980d9830293
koreavc <koreavc@DESKTOP-UOTUHMA>
2024-05-09 14:44:10 +0900

    DTO 생성 및 파일 정리

---
+++ src/main/java/com/avi6/dto/TripDTO.java
@@ -0,0 +1,26 @@
+package com.avi6.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TripDTO {
+	
+	private Long mid;
+	
+	private String nickname;
+
+	private String title;
+	
+	private LocalDateTime startDate, endDate, regDate;
+	
+	private String place;
+	
+}
